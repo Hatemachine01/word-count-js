73e84beef4f4afa8145260a84d9468b4f2c82eb2
Julio <juliodevasquez@gmail.com>
2018-04-05 21:36:16 -0400

    general refactoring

--- word-count.js
+++ word-count.js
@@ -5,14 +5,14 @@ Words.prototype.count = function( wordString ) {
 	/* Counts  times each words appears */
 	var wordCount = Object.create(null) ;
 	
-	/* Sanitizes string by removing punctuation characters. Also lower cases entire string. Trim method removes white space*/
-	wordStringSanitized = wordString.trim().toLowerCase().replace(/[.\/#!$%\^&\*;:{}=\-_~()@]/g,"")
-	
-	/* Splits sanitized string by looking for empty space, as well as commas "*/
-	wordStringSanitized.split(/\s+|,/).forEach(function(word){
+	/* Sanitizes string by removing punctuation characters. Also lower cases entire string to avoid maching errors. 
+	Trim method removes white space at both ends of the string. */
+	wordStringSanitized = wordString.trim().toLowerCase().replace(/[.\/#!$%\^&\*;:{}=\-_~()@]/g,"").split(/\s+|,/)
 	
 	/* iterates over every word and increases counter everytime it goes over a word that already exists, if the word doesn'te
 	xists, it gives its counter a value of 1 */
+	
+	wordStringSanitized.forEach(function(word){
 		if(word in wordCount && !isNaN(wordCount[word]))
 			wordCount[word]++;
 		else
